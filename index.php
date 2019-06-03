95d8c688499291c68b1ddbe57752bc2e23e445a1
Tady <meshesha1@gmail.com>
2019-06-03 21:42:32 +0300

    v1.3.3

--- index.php
+++ index.php
@@ -1138,18 +1138,41 @@ $about_html = ABOUT_APP_AUTHOR;
             var frm_id = $("#form_id").val();
             $("#form_content_id").val(frm_id);
             var form_content = get_form_content(frm_id);
-            /*
-            var tblCols = "";
+
             if(form_content != undefined && form_content != null && form_content != "new" && form_content != ""){
-                form_content_obj = JSON.parse(form_content);
-                $.each(form_content_obj, function(i,fld){
-                    if(fld.type == "table"){
-                        //console.log(fld)
-                        tblCols = fld.placeholder;
-                    }
-                })
+                var form_content_obj = JSON.parse(form_content);
+                if(form_content_obj.length > 0){
+                    form_content_obj.forEach(function(item, index){
+                        if(item.label !== undefined){
+                            item.label = item.label.replace(/&quot;/g,"\"");
+                            item.label = item.label.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.description !== undefined){ 
+                            item.description = item.description.replace(/&quot;/g,"\""); 
+                            item.description = item.description.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.type != "table" && item.placeholder !== undefined){ 
+                            item.placeholder = item.placeholder.replace(/&quot;/g,"\""); 
+                            item.placeholder = item.placeholder.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.type != "hidden" && item.value !== undefined){
+                            item.value = item.value.replace(/&quot;/g,"\"");
+                            item.value = item.value.replace(/&apos;/g, "'"); 
+                        }
+
+                        if(item.type == "select" || item.type == "checkbox-group" || item.type == "radio-group"){
+                            if(item.values !== undefined && item.values.length > 0){
+                                item.values.forEach(function(item2, index){
+                                    item2.label = item2.label.replace(/&quot;/g,"\""); 
+                                    item2.label = item2.label.replace(/&apos;/g, "'");
+                                });
+                            }
+                        }
+
+                    });
+                    form_content = JSON.stringify(form_content_obj);
+                }
             }
-            */
             var $fbEditor = $(document.getElementById('builder-editor'));
             var formBuilder;
             var positionOptions = {
@@ -1283,9 +1306,44 @@ $about_html = ABOUT_APP_AUTHOR;
                     }
                 }],
                 onSave: function (e,formData) {
-                    //console.log(formData);x
-                    formContentJsonObj = formData;
-                    setFormJsonObj(formbuilder_content_dialog);
+                    try{
+                        var formDataObj =  JSON.parse(formData);
+                        if(formDataObj.length > 0){
+                            formDataObj.forEach(function(item, index){
+                                if(item.label !== undefined){ //item.type == "paragraph" || item.type == "header" //
+                                    item.label = item.label.replace(/"/g, "&quot;"); 
+                                    item.label = item.label.replace(/'/g, "&apos;"); 
+                                }
+                                if(item.description !== undefined){ 
+                                    item.description = item.description.replace(/"/g, "&quot;"); 
+                                    item.description = item.description.replace(/'/g, "&apos;"); 
+                                }
+                                if(item.type != "table" && item.placeholder !== undefined){ 
+                                    item.placeholder = item.placeholder.replace(/"/g, "&quot;"); 
+                                    item.placeholder = item.placeholder.replace(/'/g, "&apos;"); 
+                                }
+                                if(item.type != "hidden" && item.value !== undefined){
+                                    item.value = "";
+                                }
+
+                                if(item.type == "select" || item.type == "checkbox-group" || item.type == "radio-group"){
+                                    if(item.values !== undefined && item.values.length > 0){
+                                        item.values.forEach(function(item2, index){
+                                            item2.label = item2.label.replace(/"/g, "&quot;"); 
+                                            item2.label = item2.label.replace(/'/g, "&apos;");
+                                        });
+                                    }
+                                }
+
+                            });
+                            formData = JSON.stringify(formDataObj);
+                        }
+                        //console.log(formData)
+                        formContentJsonObj = formData;
+                        setFormJsonObj(formbuilder_content_dialog);
+                    }catch(err){
+                        console.log(err.message);
+                    }
                 },
                 onOpenFieldEdit: function(editPanel) {
                     //console.log()
