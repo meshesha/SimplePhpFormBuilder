0c3fb1a40958d3db654fee66a53ba4d1734bb01f
Tady <meshesha1@gmail.com>
2019-06-05 01:36:15 +0300

    v1.3.4

--- settings/about.php
+++ settings/about.php
@@ -1,5 +1,5 @@
 <?php
-    define('APP_VERSION',"1.3.2");
+    define('APP_VERSION',"1.3.4");
     define('ABOUT_APP_AUTHOR', preg_replace("/[\r\n]*/","","
     <style>
         .about-row.content {height: 100%}
@@ -13,10 +13,12 @@
         <div class='row about-row ui-widget-content'>
             <div class='col-sm-9'>
                 <h2>About SimplePhpFormBuilder </h2>
-                <p>SimplePhpFormBuilder is a PHP Aplication that allowing you to build and management simple html forms.</p>
-                <span>Version: " . APP_VERSION . " </span><br>
-                <span>Homepage: <a href='https://github.com/meshesha/SimplePhpFormBuilder'>https://github.com/meshesha/SimplePhpFormBuilder</a> </span><br>
-                <span>License: <a href='https://github.com/meshesha/SimplePhpFormBuilder/blob/master/LICENSE' target='_blank'><span class='btn btn-primary'>MIT</span></a> </span><br>
+                <p>SimplePhpFormBuilder is a PHP Application that allowing you to build and management simple html forms.</p>
+                <hr>
+                <p><span>Version: " . APP_VERSION . "</span><span style='padding-left:10px;'><button class='btn btn-info btn-sm' onclick='checkNewVer(\\\"".APP_VERSION."\\\")' >Check for updates</button></span></p>
+                <p id='app-ver-check-result' class='ui-widget-content' style='display:none;padding:5px;'></p>
+                <p><span>Homepage: <a href='https://github.com/meshesha/SimplePhpFormBuilder'>https://github.com/meshesha/SimplePhpFormBuilder</a> </span></p>
+                <p><span>License: <a href='https://github.com/meshesha/SimplePhpFormBuilder/blob/master/LICENSE' target='_blank'><span class='btn btn-primary'>MIT</span></a> </span></p>
                 <hr>
                 <h2>About author</h2>
                 <span>Name: Tady meshesha </span><br>
