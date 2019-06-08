30de044b82e87ef2219e9852d0d058370bb86d9a
Tady <meshesha1@gmail.com>
2019-06-08 03:07:36 +0300

    v1.4.2

--- form.php
+++ form.php
@@ -71,9 +71,9 @@ if($formStatus == "2"){
 $userId = '';
 $email = '';
 $userName = '';
-if($formType == "2" || $formType == "4"){ //2-geoups not Anonymously, 4-geoups Anonymously
+if($formType == "2" || $formType == "4"){ //2-groups not Anonymously, 4-groups Anonymously
     if(isset($_SESSION['user_id'])){
-        if($formType == "2"){ //geoups not Anonymously
+        if($formType == "2"){ //groups not Anonymously
             $userId = $_SESSION['user_id'];
         }
         $records = $conn->prepare('SELECT * FROM users WHERE status="1" AND id = :userid');
@@ -286,6 +286,38 @@ function fixRGBtoRGBA($rgb){
            var form_content = get_form_content(form_id);
            //console.log(form_content);
            if(form_content != "new" && form_content != "" && form_content != null && form_content !== undefined){
+               var form_content_obj = JSON.parse(form_content);
+                if(form_content_obj.length > 0){
+                    form_content_obj.forEach(function(item, index){
+                        if(item.label !== undefined){ //item.type == "paragraph" || item.type == "header" //
+                            item.label = item.label.replace(/&quot;/g,"\"");
+                            item.label = item.label.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.description !== undefined){ 
+                            item.description = item.description.replace(/&quot;/g,"\""); 
+                            item.description = item.description.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.type != "table" && item.placeholder !== undefined){ 
+                            item.placeholder = item.placeholder.replace(/&quot;/g,"\""); 
+                            item.placeholder = item.placeholder.replace(/&apos;/g, "'"); 
+                        }
+                        if(item.type != "hidden" && item.value !== undefined){
+                            item.value = item.value.replace(/&quot;/g,"\"");
+                            item.value = item.value.replace(/&apos;/g, "'"); 
+                        }
+
+                        if(item.type == "select" || item.type == "checkbox-group" || item.type == "radio-group"){
+                            if(item.values !== undefined && item.values.length > 0){
+                                item.values.forEach(function(item2, index){
+                                    item2.label = item2.label.replace(/&quot;/g,"\""); 
+                                    item2.label = item2.label.replace(/&apos;/g, "'");
+                                });
+                            }
+                        }
+
+                    });
+                    form_content = JSON.stringify(form_content_obj);
+                }
                 $('#form-render-content').formRender({
                     dataType: 'json',
                     formData: form_content,
